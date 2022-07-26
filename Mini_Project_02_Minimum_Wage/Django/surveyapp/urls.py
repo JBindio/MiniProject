977e7e1a30f7f523070320d233b164aa10c51abf
JBindio <bindio@naver.com>
2022-07-26 22:08:12 +0900

    220726 커밋완료

--- Mini_Project_02_Minimum_Wage/Django/surveyapp/urls.py
+++ Mini_Project_02_Minimum_Wage/Django/surveyapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views as sur
 
 urlpatterns = [
-    path('test/', sur.test),
+    path('main/', sur.main),
+    path('part1_01/', sur.part1_01),
+    path('part1_02/', sur.part1_02),
+    path('part1_03/', sur.part1_03),
 ]
--- Mini_Project_02_Minimum_Wage/Django/surveyapp/views.py
+++ Mini_Project_02_Minimum_Wage/Django/surveyapp/views.py
@@ -2,7 +2,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
-def test(request):
-    return HttpResponse('테스트')
+# -------------------------------------------------------------------------------------------
+# 메인 페이지
+def main(request):
+    return render(request,
+           'surveyapp/main.html',
+           {})
+# -------------------------------------------------------------------------------------------
+# Part1_01 연도별 최저임금 현황
+def part1_01(request):
+    return render(request,
+           'surveyapp/part1_01.html',
+           {})
+# -------------------------------------------------------------------------------------------
+# Part1_02 연도별 물가 현황    
+def part1_02(request):
+    return render(request,
+           'surveyapp/part1_02.html',
+           {})
+# -------------------------------------------------------------------------------------------
+# Part1_01 연도별 GDP 현황
+def part1_03(request):
+    return render(request,
+           'surveyapp/part1_03.html',
+           {})
+# -------------------------------------------------------------------------------------------
